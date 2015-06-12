e2dd09b32fd7607e057044fe80b68464f086d5c3
Mike Ruthven <mike.ruthven@d2l.com>
2015-06-12 10:44:59 -0400

    ADD: superagent-promise lib CHG: Get Forum Name and Topic name for each post, display in table

--- discussions/main.js
+++ discussions/main.js
@@ -1,4 +1,4 @@
-var request = require('superagent'),
+var request = require('superagent-promise')(require('superagent'), this.Promise || require('promise')),
     auth = require('superagent-d2l-session-auth');
 
 request
@@ -6,15 +6,38 @@ request
     .use(auth)
     .end(function(err, res) {
         var posts = res.body;
+        var promises = [];
+        for (var i = 0; i < posts.length; i++) {
+        	var post = posts[i]
+        	promises.push(request
+        	  .get('/d2l/api/le/1.5/115495/discussions/forums/' + post.ForumId)
+        	  .use(auth)
+        	  .then(function(res) {
+        	  	post.ForumName = res.body.Name;
+        	  }));
+        	promises.push(request
+        		.get('/d2l/api/le/1.5/115495/discussions/forums/' + post.ForumId + '/topics/' + post.TopicId)
+        		.use(auth)
+        		.then(function(res) {
+        			post.TopicName = res.body.Name;
+        		}));
+        } 
+        Promise.all(promises)
+         .then(function() {
         var postsTable = document.getElementById("posts");
-        for (var i=0, tr, td; i < posts.length; i++) {
-        	tr = document.createElement('tr');
-        	td = document.createElement('td');
-        	td.appendChid(document.createTextNode(posts[i].ForumId));
-        	tr.appendChild(td);
-        	td = document.createElement('td');
-        	td.appendChid(document.createTextNode(posts[i].Message.Html));
-        	tr.appendChild(td);
+				for (var i=0, tr, td, post; i < posts.length; i++) {
+					post = posts[i];
+					tr = document.createElement('tr');
+					td = document.createElement('td');
+					td.appendChild(document.createTextNode(post.ForumName));
+					tr.appendChild(td);
+					td = document.createElement('td');
+					td.appendChild(document.createTextNode(post.TopicName));
+					tr.appendChild(td);
+					td = document.createElement('td');
+					td.innerHTML = post.Message.Html;
+					tr.appendChild(td);
 					postsTable.appendChild(tr);
-        }
-    });
+		    }
+		  })});
+
